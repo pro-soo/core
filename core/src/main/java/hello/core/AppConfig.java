e1321bcc0bdd7aa8736102f30abb59852cec6436
pro-soo <rhackdtn@naver.com>
2025-06-30 07:48:08 +0900

    app config 리팩토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,37 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+/**
+ * 구성 영역
+ * : 역할과 구현을 쉽게 파악 가능 + 중복 제거
+ * 사용 영역은 수정할 필요가 X
+ */
 public class AppConfig {
 
     public MemberService memberService(){
         // 생성자 주입 (Injection)
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
