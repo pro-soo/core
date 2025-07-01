277d6889f15edbe525db300c08139b7c9e6d289f
pro-soo <rhackdtn@naver.com>
2025-07-01 22:40:39 +0900

    스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,28 +9,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 구성 영역
  * : 역할과 구현을 쉽게 파악 가능 + 중복 제거
  * 사용 영역은 수정할 필요가 X
  */
+@Configuration
 public class AppConfig {
 
-    public MemberService memberService(){
+    @Bean // 스프링 컨테이너에 등록된다
+    public MemberService memberService() {
         // 생성자 주입 (Injection)
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    public OrderService orderService(){
+    @Bean
+    public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    public DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
