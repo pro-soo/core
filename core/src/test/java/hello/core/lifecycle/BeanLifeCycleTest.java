87b56549d634abfd5826ed9389e631bc57314e79
pro-soo <rhackdtn@naver.com>
2026-04-13 17:07:26 +0900

    8.빈 생명주기 콜백

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new
+                AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
+    }
+    @Configuration
+    static class LifeCycleConfig {
+//        @Bean(initMethod = "init", destroyMethod = "close") -> 코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 할 경우 사용
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
